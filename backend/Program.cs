26c49307a9879c86aecfbe6e83a1b070520648fc
Ансар Аширбеков <ans962011@gmail.com>
2026-08-19 22:34:44 +0500

    fixing deploy2

--- backend/Program.cs
+++ backend/Program.cs
@@ -22,19 +22,19 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
     {
         options.Cookie.Name = "task4_auth";
         options.Cookie.SameSite = SameSiteMode.None;
-        options.Cookie.Name = "task4_auth";
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        options.Cookie.HttpOnly = true;
+        options.Cookie.IsEssential = true;
 
         options.Events.OnRedirectToLogin = context =>
         {
             context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-
             return Task.CompletedTask;
         };
 
         options.Events.OnRedirectToAccessDenied = context =>
         {
             context.Response.StatusCode = StatusCodes.Status403Forbidden;
-
             return Task.CompletedTask;
         };
     }
